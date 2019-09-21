8bcf54db1e98e690b1f8dfa537fd6fdaa2b6a2de
alessanderleite <alessander.logistica@gmail.com>
2019-09-21 17:53:21 -0300

    create the class named AlunoDAO

--- src/br/com/alessanderleite/dao/AlunoDAO.java
+++ src/br/com/alessanderleite/dao/AlunoDAO.java
@@ -1,11 +1,34 @@
 package br.com.alessanderleite.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.alessanderleite.connection.ConnectionFactory;
 import br.com.alessanderleite.model.Aluno;
 
 public class AlunoDAO {
 	
 	public void create(Aluno aluno) {
 		
+		try {
+			ConnectionFactory connectionFactory = new ConnectionFactory();
+			Connection conn = connectionFactory.createConnection();
+			
+			String sql = "INSERT INTO aluno (id, nom, inst, gen) values(?, ?, ?, ?)";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			
+			stmt.setLong(1, aluno.getMatricula());
+			stmt.setString(2, aluno.getNome());
+			stmt.setString(3, aluno.getInstituicao());
+			stmt.setBoolean(4, aluno.getSexo());
+			
+			stmt.execute();
+			stmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
